f1499ba63a26549eb4cf2e1ec8b03e27de685462
Giovanna Laís <gi.laisoliveiraa@icloud.com>
2025-04-07 18:24:28 -0300

    🚧 (src/Form/RegisterForm): Incluindo campos no formulario de Registro

--- src/components/Form/RegisterForm.tsx
+++ src/components/Form/RegisterForm.tsx
@@ -1,19 +1,70 @@
 import styles from "./Form.module.scss";
-import { TextField, Button, InputAdornment, IconButton, OutlinedInput, InputLabel, FormControl, Box } from "@mui/material";
+import { TextField, Button, InputAdornment, IconButton, OutlinedInput, InputLabel, FormControl, Box, Select, MenuItem, SelectChangeEvent } from "@mui/material";
 import EmailIcon from '@mui/icons-material/Email';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AssignmentIcon from '@mui/icons-material/Assignment';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import DateRangeIcon from '@mui/icons-material/DateRange';
 import { useState } from "react";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import  ptBR  from 'date-fns/locale/pt-BR';
 
 const RegisterForm = () => {
+
   const [showPassword, setShowPassword] = useState(false);
+  const [sexo, setSexo] = useState('');
+  const [birthDate, setBirthDate] = useState<Date | null>(null);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleChange = (event: SelectChangeEvent) => {
+    setSexo(event.target.value as string);
+  };
 
   return (
-    <Box className={styles.form}>
+    <Box className={styles.formRegister}>
       <TextField
-        fullWidth
+        label="Nome"
+        variant="outlined"
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircleIcon />
+            </InputAdornment>
+          )
+        }}
+        sx={{width: '400px'}}
+      />
+      <TextField
+        label="Sobrenome"
+        variant="outlined"
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircleIcon />
+            </InputAdornment>
+          )
+        }}
+        sx={{width: '400px'}}
+      />
+      <TextField
+        label="CPF"
+        variant="outlined"
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AssignmentIcon />
+            </InputAdornment>
+          )
+        }}
+        sx={{width: '400px'}}
+      />
+      <TextField
         label="Email"
         variant="outlined"
         size="small"
@@ -26,8 +77,7 @@ const RegisterForm = () => {
         }}
         sx={{width: '400px'}}
       />
-      <br></br>
-      <FormControl variant="outlined" fullWidth size="small">
+      <FormControl variant="outlined" size="small">
         <InputLabel htmlFor="password">Password</InputLabel>
         <OutlinedInput
           id="password"
@@ -47,7 +97,28 @@ const RegisterForm = () => {
           sx={{width: '400px'}}
         />
       </FormControl>
-      <br></br>
+      <FormControl sx={{width: '400px'}}>
+        <InputLabel id="demo-simple-select-label">Sexo</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={sexo}
+          label="Sexo"
+          onChange={handleChange}
+        >
+          <MenuItem value={'F'}>Feminimo</MenuItem>
+          <MenuItem value={'M'}>Masculino</MenuItem>
+        </Select>
+      </FormControl>
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
+        <DatePicker
+          label="Data de Nascimento"
+          value={birthDate}
+          onChange={(newValue) => setBirthDate(newValue)}
+          sx={{width: '400px'}}
+        />
+      </LocalizationProvider>
+
       <Button className="btnPrimary">
         Registrar
       </Button>
