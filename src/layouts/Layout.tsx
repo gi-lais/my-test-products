3bfe90c16c151beea1ce461743159899d3664068
Giovanna Laís <gi.laisoliveiraa@icloud.com>
2025-04-08 00:11:29 -0300

    🚧 💄(src/layouts): Atualizando Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,19 +1,18 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "../components/Sidebar/Sidebar";
+import { Box } from "@mui/material";
 import Header from "../components/Header/Header";
-import styles from "./ProtectedLayout.module.scss";
+import Sidebar from "../components/Sidebar/Sidebar";
 
-const ProtectedLayout = () => {
+const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={styles.container}>
-      <Sidebar />
-      <div className={styles.content}>
-        <Header />
-        <main className={styles.main}>
-          <Outlet />
-        </main>
-      </div>
-    </div>
+    <Box display="flex" flexDirection="column" height="100vh">
+      <Header />
+      <Box display="flex" flex={1}>
+        <Sidebar />
+        <Box flex={1} p={3} bgcolor="#f5f5f5">
+          {children}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
