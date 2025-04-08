0ce43e2075da04502dc3d952f36703179e9f3827
Giovanna Laís <gi.laisoliveiraa@icloud.com>
2025-04-08 00:03:16 -0300

    ✨ (src/routes): Criando rota private

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "../pages/Auth/Auth";
 import Products from "../pages/Products/Products";
+import { PrivateRoute } from "./PrivateRoute";
+import ProtectedLayout from "../layouts/Layout";
 
 export const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Auth />} />
-      <Route path="/products" element={<Products />} />
+      <Route
+        path="/products"
+        element={
+          <PrivateRoute>
+            <ProtectedLayout />
+          </PrivateRoute>
+        }
+      >
+        <Route index element={<Products />} />
+      </Route>
+      <Route path="*" element={<div>Página não encontrada</div>} />
     </Routes>
   </BrowserRouter>
 );
