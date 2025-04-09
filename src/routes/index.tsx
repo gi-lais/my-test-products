a0cc562bdf93012b9a3c0cf69e450f44c1ec5cdc
Giovanna Lais <gi.laisoliveiraa@icloud.com>
2025-04-08 22:15:20 -0300

    🚧 (src/routes): Adicionando novas rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import Auth from "../pages/Auth/Auth";
 import Products from "../pages/Products/Products";
 import { PrivateRoute } from "./PrivateRoute";
 import ProtectedLayout from "../layouts/Layout";
+import ProductForm from "../components/ProductForm/ProductForm";
 
 export const AppRoutes = () => (
   <BrowserRouter>
@@ -18,6 +19,29 @@ export const AppRoutes = () => (
           </PrivateRoute>
         }
       />
+
+      <Route
+        path="/products/create"
+        element={
+          <PrivateRoute>
+            <ProtectedLayout>
+              <ProductForm />
+            </ProtectedLayout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/products/edit/:id"
+        element={
+          <PrivateRoute>
+            <ProtectedLayout>
+              <ProductForm isEditMode />
+            </ProtectedLayout>
+          </PrivateRoute>
+        }
+      />
+
       <Route path="*" element={<div>Página não encontrada</div>} />
     </Routes>
   </BrowserRouter>
