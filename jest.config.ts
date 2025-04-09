0c668536d2f2f247f827d6f918efb3242e966c37
Giovanna Laís <gi.laisoliveiraa@icloud.com>
2025-04-09 16:45:42 -0300

    ✨🧪 Adicionando configurações de teste, com jest e testing library

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.jest.json",
+    },
+  },
+};
+
+export default config;
