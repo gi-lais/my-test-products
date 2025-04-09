d087ed61b19a6ce43cb7ca1b0f8f20a3a441ab8b
Giovanna Laís <gi.laisoliveiraa@icloud.com>
2025-04-09 08:10:22 -0300

    ✨ (src/pages/NotFound): Criando page Not Found

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,22 @@
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <img
+        src="src/assets/notFound.jpg"
+        alt="Imagem representando pagina não encontrada"
+        style={{ width: "650px" }}
+      />
+      <Typography className="text-subtitle">Pagina não encontrada!</Typography>
+      <Button className="btnPrimary" onClick={() => navigate("/")}>
+        Voltar
+      </Button>
+    </Box>
+  );
+};
+export default NotFound;
