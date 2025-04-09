6402050015ed765e09bc67bdfa57f81f0d16b4d1
Giovanna Laís <gi.laisoliveiraa@icloud.com>
2025-04-09 08:10:50 -0300

    ➕ Adicionando LIBS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/reset.css";
 import "./styles/_global.scss";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
